ae6058891e198285cc703473b7e9b3efdfc667e5
Ashik <ashikurashik20999@gmail.com>
2026-05-31 07:39:15 +0600

    fb service key add

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const uri = process.env.MONGO_URL;
 
-const serviceAccount = require("./decor-nest-firebase-adminsdk.json");
+// const serviceAccount = require("./decor-nest-firebase-adminsdk.json");
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf8');
+const serviceAccount = JSON.parse(decoded);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
